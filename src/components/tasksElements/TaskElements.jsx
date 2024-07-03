d77070b336151aba40a5f61b242f4900d2781d74
Alistair Abao <abaolester@gmail.com>
2024-07-03 15:48:19 +0200

    implemented modal ad displaying of task data using useEffect in the modal component

--- src/components/tasksElements/TaskElements.jsx
+++ src/components/tasksElements/TaskElements.jsx
@@ -3,12 +3,16 @@ import Wrapper from "../../assets/wrappers/TaskElementWrapper.js";
 /** Useravatar to display to each of the tasks */
 import UserAvatar from "../../components/navigationBar/UserAvatar.jsx";
 
+// import { useState } from "react";
+
 import { AiOutlineExclamationCircle } from "react-icons/ai";
 
-function TaskElements({ task, avatar, category }) {
+function TaskElements({ task, avatar, category, click }) {
+  /** State to handle the rendering of the modal when a task is clicked */
+  // const [isOpen, setIsOpen] = useState(false);
   return (
     <Wrapper>
-      <div className='content-element'>
+      <div className='content-element' onClick={click}>
         {/* each of the box elements */}
         <div className='each-element'>
           <span className='userAvatar'>
